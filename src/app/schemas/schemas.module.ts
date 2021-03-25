6bf9b965c68574aab59d55891780013452cd5d97
Eli Howey <elihowey@gmail.com>
2021-03-25 04:52:24 -0400

    refactor: :recycle: move schemas into library

--- src/app/schemas/schemas.module.ts
+++ src/app/schemas/schemas.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import FormControlSchema from '../../../schemas/form-control.schema.json';
-import FormMetadataSchema from '../../../schemas/form-metadata.schema.json';
-import FormQuestionSchema from '../../../schemas/form-question.schema.json';
-import FormSectionSchema from '../../../schemas/form-section.schema.json';
-import IdSchema from '../../../schemas/properties/id.schema.json';
+import FormControlSchema from '../../../projects/dynamic-form/schemas/form-control.schema.json';
+import FormMetadataSchema from '../../../projects/dynamic-form/schemas/form-metadata.schema.json';
+import FormQuestionSchema from '../../../projects/dynamic-form/schemas/form-question.schema.json';
+import FormSectionSchema from '../../../projects/dynamic-form/schemas/form-section.schema.json';
+import IdSchema from '../../../projects/dynamic-form/schemas/properties/id.schema.json';
 import { ALL_SCHEMAS, ENTRY_SCHEMA } from './schemas.token';
 
 const schemas = [FormMetadataSchema, FormSectionSchema, FormQuestionSchema, FormControlSchema, IdSchema];
